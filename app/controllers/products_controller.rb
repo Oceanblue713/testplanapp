a3c192681eb703d8c0140fcdcadd5920bcd534e3
Anri Liy <amedamachip713@gmail.com>
2018-05-04 13:59:07 -0500

    product testplan

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,7 +30,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       if @product.save
         puts "success"
-        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         puts "failed"
